c74eeb3bddc1181db11eaa1a1fb34b06bd36e2cb
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-04-15 12:47:27 +0600

    update

--- application/config/database.php
+++ application/config/database.php
@@ -1,18 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	
-  	'username' => 'root',
-  	'password' => '',
-  	// 'username' => 'bigektuw',
-  	// 'password' => 'sChzA2HnVel0',
-  	'database' => 'bigektuw_anabiaenterprise',
 
-		
+	'username' => 'root',
+	'password' => '',
+	// 'username' => 'bigektuw',
+	// 'password' => 'sChzA2HnVel0',
+	'database' => 'bigektuw_anabiaenterprise',
+
+
 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
